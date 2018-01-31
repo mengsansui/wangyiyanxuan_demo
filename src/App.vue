ecc5be05b3ef583d5169685a798ea28202f483c1
mengsansui <912114405@qq.com>
2018-01-31 12:33:43 +0800

    项目

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app">
+    <div class="tabbar">
+        <router-link to="/home">
+            <a href="">
+               <img src="../Appimg/footer1-1.jpg">
+               <div>首页</div>
+            </a> 
+        </router-link>
+        <router-link to="/market">
+           <a href="">
+               <img src="../Appimg/footer2-1.jpg">
+               <div>分类</div>
+           </a>
+        </router-link>
+        <router-link to="/cart">
+            <a href="">
+               <img src="../Appimg/footer3-1.jpg">
+               <div>购物车</div>
+            </a>
+        </router-link>
+        <router-link to="/mine">
+            <a href="">
+                <img src="../Appimg/footer4-1.jpg">
+               <div style="margin-left:-0.0185rem;">个人</div>
+            </a>
+        </router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted(){
+			document.documentElement.style.fontSize = innerWidth / 10 + "px";
+			window.onresize = function() {
+				document.documentElement.style.fontSize = innerWidth / 10 + "px";
+			}
+    }
+}
+</script>
+
+<style>
+*{box-sizing:border-box;}
+body,nav,dl,dt,dd,p,h1,h2,h3,h4,ul,ol,li,input,button,textarea,footer{margin:0;padding:0}
+body{font:16px/1.5 'Microsoft Yahei','Simsun';  color:#333;background:#fff;-webkit-text-size-adjust: none; min-width:320px;}
+h1,h2,h3,h4,h5,h6{font-size:100%}
+ul,ol{list-style:none}
+a{text-decoration:none;color:#1a1a1a}
+a:hover, a:active, a:focus{color:#1c5aa2;text-decoration: none;}
+a:active{color:#aaa;}
+img{vertical-align:middle;border:0;-ms-interpolation-mode:bicubic;}
+button,input,select,textarea{font-size:100%; vertical-align:middle;outline:none;}
+textarea{resize:none}
+button,input[type="button"],input[type="reset"],input[type="submit"] {cursor:pointer;-webkit-appearance:button;-moz-appearance:button}
+input:focus:-moz-placeholder,input:focus::-webkit-input-placeholder {color:transparent}
+button::-moz-focus-inner,input::-moz-focus-inner { padding:0; border:0}
+table { border-collapse:collapse; border-spacing:0}
+/*.fl{float:left;}.fr{float:right;}.hide{display:none;}.show{display: block;}*/
+.ellipsis { white-space:nowrap; text-overflow:ellipsis; overflow:hidden}
+.break { word-break:break-all; word-wrap:break-word}
+header, footer, article, section, nav, menu, hgroup {display: block;clear:all;}
+/*清浮动*/
+.clear:after {
+    content: '';
+    display: block;
+    clear: both;
+}
+
+#app .tabbar{
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 1.30667rem;
+  background-color: #fafafa;
+  font-size: .32rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.tabbar a div:hover {
+  color: #b4282d;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+// 配置路由的js
+import Vue from 'vue'
+// 导入路由模块
+import VueRouter from 'vue-router'
+// 全局安装一下路由模块
+Vue.use(VueRouter)
+
+// 分别导入四个组件
+import Home from '../components/Home'
+import Market from '../components/Market'
+import Cart from '../components/Cart'
+import Mine from '../components/Mine'
+
+// 创建路由实例，并且配置路由规则
+export default new VueRouter({
+  // 这里写路由规则
+  routes: [
+    // 这个path就是router-link里的to跳转的地址
+    // component就是根据路由地址，找到对应的组件，显示到router-view中
+    { path: '', component: Home },
+    { path: '/home', component: Home },
+    { path: '/market', component: Market },
+    { path: '/cart', component: Cart },
+    { path: '/mine', component: Mine }
+  ]
+})
