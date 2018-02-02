f766ed0c449e991a2031552a35771b8d8eca964b
zhangxin <763335917@qq.com>
2018-02-02 15:31:04 +0800

    three ti

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -65,9 +65,208 @@
             <div class="specSelect">
                 积分:购买最高得99积分
             </div>
-            <div class="specSelect">
+
+        </div>
+        <div class="bai">
+
+        </div>
+        <div class="specSelects ">
+            <div class="specSelect tty">
+                用户评价(149)
+                <span class="ttu">96.6%  好评</span>
+            </div>
+
+
+        </div>
+        <div class="bai">
+
+        </div>
+
+        <div class="specSelects ">
+            <div class="specSelect tty ytt">
+                商品参数
 
             </div>
+            <div class="specSelect tty ytt">
+                <span class="chan1">产地</span>
+                <span class="ttu utt">日本</span>
+
+            </div>
+            <div class="specSelect tty ytt">
+                <span class="chan1">项链</span>
+                <span class="ttu utt">18K金</span>
+
+            </div>
+            <div class="specSelect tty ytt">
+                <span class="chan1">珍珠</span>
+                <span class="ttu utt">天然海水珠</span>
+
+            </div>
+            <div class="specSelect tty ytt">
+                <span class="chan1">珍珠形状</span>
+                <span class="ttu utt">正圆</span>
+
+            </div>
+            <div class="specSelect tty ytt">
+                <span class="chan1">珍珠光泽</span>
+                <span class="ttu utt">强光</span>
+
+            </div>
+            <div class="specSelect tty ytt">
+                <span class="chan1">珍珠颜色</span>
+                <span class="ttu utt">白透粉</span>
+
+            </div>
+            <div class="specSelect tty ytt">
+                <span class="chan1">珍珠小贴士</span>
+                <span class="ttu utt uui">因严选珍珠取自海洋蚌非人造珍珠，故形状，光泽，颜色上会有细微差异；也无法做到人为意义上的完美无瑕。</span>
+
+            </div>
+
+
+
+        </div>
+        <div class="tui">
+            <img src="../assets/96ae01bfeb766a47842b5133d6ff36d5.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/688627b79dd503886a5d212ecfd9fadf.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/bdd483b598a859d29d899c55ac0979b4.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/273aaa16a906945ba3102790e173e759.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/17a6f125ded17f9aa27a46a9316856ac.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/e39998478805f60996c5dc329bfb8528.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/0924211d37b131b98d5cb90869f36fd9.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/add8b2a8d21e5fd0e5e2d15677082dc4.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/6e0d81837e511ca7ba37c65ba68ef423.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/aa2910579d806fa83b0a5381d5cd3f3a.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/7752d3198023fa68ee3041c4bde8d152.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/351600dd8d8f9fee745ba5087c9181df.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/15d1f8b4314d671ec284631c48e88ea1.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/324717c5a9b6c708f95a242732fa642f.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/b56fbe823aa911e0d014426dbf6cb065.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/a19fdb4674a5c25c116d7a8375e9b960.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/33f119e203e732585635999bc01597ba.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/0610c64aa4aa0d6fbc1a6e1fdd698577.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/c434ef76f690c2a8fdebe5456a253c96.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/03085fdac2a5da7a354ec76a5e2afb2c.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/16b52598ae9968ac2d373dba683cd4d4.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/a593d19a570e657f8cd77fe568d9e6c9.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/9dc4e0892236e5fb8cf05719bb2d6b6b.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/0882b97a051c0ae60648948a52f6f996.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/1470d49e170d94daa93db0a78a7a477d.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/82906dc91ef7e51ce8f9318292e304ae.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/3a81fae9d9caf498fe5c76b5ce7d0c23.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/f2affc282607f348cec0d52bf518dafb.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/65c9d2ea29907abad396857ecd1ce56a.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/14628939c02c5017ebe802c00b23020b.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/409e4c1eed1230bec160ebf8edef41fb.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/d3345f4af4511a0ab05d47afb87a6722.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/e6c5a803090bd75e8700d8a644a3731a.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/db47b95c364102b865ae6d133041797a.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/6e2401b9d39f80ab9f8e12a5427db341.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/5c7fa2685d79cba0aff001877f9c1ac6.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/00c210975d6634c4382f062cf6a9c4f1.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/06e0e9af992e6fbfc7a80400e10c2a7a.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/966eed0cb60e8ccfd42343d558f4658b.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/ab502ffe0118621b4eefa57d3ddd422f.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/f8f6f1d0fc6d8c52b8bbd0de84952e70.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/215f809960a47711850120eeb69650aa.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/d0befcaf010af3378ea802fbdda8e072.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/ec89de34380b880f865ea0ef1a66fd55.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/6ebee891dbbe11e93e783ba1ff9a21df.jpg" alt="">
+        </div>
+        <div class="tui">
+            <img src="../assets/b19c539e87b7cc5f4d1dbccc82b93402.jpg" alt="">
+        </div>
+
+
+        <div class="gaos">
 
         </div>
 
@@ -207,5 +406,43 @@ export default {
 .specSelect{
     width: 95%;
     margin-left: .5rem;
+    height: 1rem;
+    line-height: 1rem;
+    border-bottom: .01rem solid gray;
+    font-size: .4rem;
+}
+.gaos{
+    width: 100%;
+    height: 5.3263rem;
+    background: #fff;
+}
+.tty .ttu{
+    float: right;
+    margin-right: .5rem;
+}
+.ytt{
+    border-bottom: .01rem dashed gray;
+    color: #999;
+}
+.ytt .chan1{
+    width: 2rem;
+}
+.ytt .utt{
+    margin-right:4.5rem;
+    color:#333;
+    text-align: left;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}
+.ytt .uui{
+    margin-right: 0;
+    width: 6rem;
+}
+.tui{
+    width: 100%;
+}
+.tui img{
+    width: 100%;
 }
 </style>
